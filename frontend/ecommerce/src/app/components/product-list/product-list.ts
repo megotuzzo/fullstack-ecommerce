fca9923d6f6d364ab42ee67e4245e1370468a940
Maria Eduarda Gotuzzo da Silva <m.gotuzzo@edu.pucrs.br>
2025-09-10 16:07:54 -0300

    layout + products table

--- frontend/ecommerce/src/app/components/product-list/product-list.ts
+++ frontend/ecommerce/src/app/components/product-list/product-list.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product';
 import { Product } from '../../common/product';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
-  templateUrl: './product-list.html',
+  imports: [CommonModule],
+  templateUrl: './product-list-table.html',
+  //templateUrl: './product-list.html',
   styleUrl: './product-list.scss'
 })
 export class ProductList implements OnInit{
