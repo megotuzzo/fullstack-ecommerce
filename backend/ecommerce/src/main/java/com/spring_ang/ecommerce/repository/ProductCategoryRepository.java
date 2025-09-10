afa9216f4600c9f26770d2b2df7bf35ea9d25923
Maria Eduarda Gotuzzo da Silva <m.gotuzzo@edu.pucrs.br>
2025-09-10 17:17:16 -0300

    cross origin config

--- backend/ecommerce/src/main/java/com/spring_ang/ecommerce/repository/ProductCategoryRepository.java
+++ backend/ecommerce/src/main/java/com/spring_ang/ecommerce/repository/ProductCategoryRepository.java
@@ -2,9 +2,11 @@ package com.spring_ang.ecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.spring_ang.ecommerce.entity.ProductCategory;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource( collectionResourceRel="productCategory", path="product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long>{
     
